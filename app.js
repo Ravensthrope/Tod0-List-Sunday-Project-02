761dd64f48875bb11b0c695cc24003eaf515217b
Aafaq Sayed <sdaafaq@gmail.com>
2023-09-06 20:41:23 +0530

    app.js functionalities are completed

--- app.js
+++ app.js
@@ -51,22 +51,36 @@ function addTodo(text) {
 
 //Remove function
 function removeTodo(id) {
-
+    todoItems = todoItems.filter(todo => todo.id !== Number(id))
+    saveAndRender()
 }
 
 //Mark as completed
 function markAsCompleted(id) {
-
+    todoItems = todoItems.filter(todo => {
+        if(todo.id === Number(id)) {
+            todo.completed = true
+        }
+        return todo
+    })
+    audio.play()
+    saveAndRender()
 }
 
 //Mark as inCompleted(id)
 function markAsIncompleted(id) {
-
+    todoItems = todoItems.filter(todo => {
+        if(todo.id === Number(id)) {
+            todo.completed = false
+        }
+        return todo
+    })
+    saveAndRender()
 }
 
 //Save in local storage
-function saveInLocStor() {
-
+function save() {
+    localStorage.setItem('todoItems', JSON.stringify(todoItems))
 }
 
 // Render function to display tasks on the page
@@ -107,5 +121,39 @@ function saveAndRender() {
 
 //create todo list item
 function createTodoElement(todo) {
+    //Creating todo list container
+    const todoDiv = document.createElement('div')
+    todoDiv.setAttribute('data-id', todo.id)
+    todoDiv.className = "todo-item"
 
+    //Creating todo item text
+    const todoTextSpan = document.createElement('span')
+    todoTextSpan.innerHTML = todo.text
+
+    //Checkbox for list
+    const todoInputCheckbox = document.createElement('input')
+    todoInputCheckbox.type = 'checkbox'
+    todoInputCheckbox.checked = todo.completed
+    todoInputCheckbox.onclick = (e) => {
+        let id = e.target.closest('.todo-item').dataset.id
+        e.target.checked ? markAsCompleted(id) : markAsIncompleted(id)
+    }
+
+    //Delete button for list
+    const todoRemoveBtn = document.createElement('a')
+    todoRemoveBtn.href = '#'
+    todoRemoveBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M18 6l-12 12"></path>
+    <path d="M6 6l12 12"></path>
+ </svg>`
+    todoRemoveBtn.onclick = (e) => {
+        let id = e.target.closest('.todo-item').dataset.id
+        removeTodo(id)
+    }
+    todoTextSpan.prepend(todoInputCheckbox)
+    todoDiv.appendChild(todoTextSpan)
+    todoDiv.appendChild(todoRemoveBtn)
+
+    return todoDiv
 }
